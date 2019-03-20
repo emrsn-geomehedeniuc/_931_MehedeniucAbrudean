653833121e056b3e9bf7fbfea3a0bd624f778562
Geo Mehedeniuc <gheorghe.mehedeniuc@gmail.com>
2019-03-20 10:48:15 +0200

    Initial commit

---
+++ src/main/java/931_MehedeniucAbrudeanMV/App.java
@@ -0,0 +1,13 @@
+package 931_MehedeniucAbrudeanMV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
